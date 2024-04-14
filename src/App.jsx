d20b51da4bada15c92370f3a02f15fa1dd078ee6
Mouhamad-Shaker-1 <mouhamadshaker073@gmail.com>
2024-04-14 09:49:12 -0500

    add the title to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import reactLogo from "./assets/react.svg";
+// import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 import { nanoid } from "nanoid";
